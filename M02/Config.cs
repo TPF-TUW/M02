a2f8e2113d22082df56d30219fe41864fc7c84ed
charaph <72637175+charaph@users.noreply.github.com>
2020-10-19 10:14:47 +0700

    Update Path

--- M02/Config.cs
+++ M02/Config.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 public class Config
 {
-    public const string PATH_FILE_CONNECTION = @"\\172.16.0.190\MDS_Project\FileConfig\Configue.ini";
-    public const string CONNECTION_SECTION = "ConnectionString";
-    public static string DEF_SERVER { get; set; }
-    public static string DEF_DB { get; set; }
-    public static string DEF_USER { get; set; }
-    public static string DEF_PW { get; set; }
+    //public const string PATH_FILE_CONNECTION = @"\\172.16.0.190\MDS_Project\FileConfig\Configue.ini";
+    //public const string CONNECTION_SECTION = "ConnectionString";
+    //public static string DEF_SERVER { get; set; }
+    //public static string DEF_DB { get; set; }
+    //public static string DEF_USER { get; set; }
+    //public static string DEF_PW { get; set; }
 
 
 }
